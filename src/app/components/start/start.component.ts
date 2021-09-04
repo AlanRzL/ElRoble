ef57616e8ff906c7b2a1ddac8a6fa6b47185c058
jhon <jhonjtoloza@gmail.com>
2021-09-04 11:30:46 -0500

    facebook login

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { UserModel } from 'src/app/interfaces/interfaces';
-import { AuthService } from 'src/app/services/auth.service';
-import { SessionService } from 'src/app/services/session.service';
-
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {UserModel} from 'src/app/interfaces/interfaces';
+import {AuthService} from 'src/app/services/auth.service';
+import {SessionService} from 'src/app/services/session.service';
+import {Facebook, FacebookLoginResponse} from "@ionic-native/facebook/ngx";
+import {AlertController} from "@ionic/angular";
 
 
 @Component({
@@ -17,19 +18,22 @@ export class StartComponent implements OnInit {
 
   constructor(
     private sessionService: SessionService,
-    private authService:AuthService,
-    private router:Router
-    ){ }
+    private authService: AuthService,
+    private router: Router,
+    private readonly facebook: Facebook,
+    private readonly alert: AlertController
+  ) {
+  }
 
   ngOnInit() {
 
     this.user = this.sessionService.getLocalUser();
     console.log(this.user);
-    if(this.user){
+    if (this.user) {
       console.log("autenticado");
       this.authService.setAuth(true);
       this.router.navigateByUrl('/tabs/tab1');
-    }else{
+    } else {
       console.log("no autenticado");
       this.authService.setAuth(false);
     }
@@ -37,37 +41,47 @@ export class StartComponent implements OnInit {
   }
 
 
+  async loginFacebook() {
+    await this.facebook.logout();
+    this.facebook.login(['email','public_profile','user_link','user_photos']).then((value: FacebookLoginResponse) => {
+      console.log("Login success", value);
+      this.facebook.getCurrentProfile().then(profile => {
+        console.log(profile);
+      });
+      this.facebook.api('me?fields=id,name,email,picture.width(400).height(400)',[]).then(async pic => {
+        console.log(pic)
+        const alert = await this.alert.create({
+          message: JSON.stringify(pic)
+        })
+        alert.present()
+      })
+    }).catch(err => {
+      console.log("Acurrio un error al intertar logearse", err)
+    })
 
-
-
-
-
-  loginFacebook(){
-    this.authService.setAuth(true);
-    let autenticado = this.authService.isAuthFunction();
-    if(autenticado){
-      console.log("autenticado");
-      this.router.navigateByUrl('/tabs/tab1');
-    } else{
-      console.log("no autenticado");
-    }
+    // this.authService.setAuth(true);
+    // let autenticado = this.authService.isAuthFunction();
+    // if(autenticado){
+    //   console.log("autenticado");
+    //   this.router.navigateByUrl('/tabs/tab1');
+    // } else{
+    //   console.log("no autenticado");
+    // }
   }
 
 
+  loginGuest() {
 
-  loginGuest(){
-    
     /*this.authService.setAuth(false);
     this.router.navigateByUrl('/tabs/tab1');*/
-    
 
 
     this.authService.setAuth(true);
     let autenticado = this.authService.isAuthFunction();
-    if(autenticado){
+    if (autenticado) {
       console.log("autenticado");
       this.router.navigateByUrl('/tabs/tab1');
-    } else{
+    } else {
       console.log("no autenticado");
     }
   }
